75f8ca3559c3dfdb0b40a519031a3c35192c1b08
Olga <olga.volotovich@mail.ru>
2025-04-28 23:01:48 +0200

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createWriteStream, createReadStream } from "fs";
+import { createGzip } from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream("src/zip/files/fileToCompress.txt");
+  const writeStream = createWriteStream("src/zip/files/archive.gz");
+
+  readStream.pipe(createGzip()).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log("File compressed and saved");
+  });
 };
 
 await compress();
