b77d00a7c22cc442bafd7b52cb79abc67b86477a
Olga <olga.volotovich@mail.ru>
2025-04-28 23:22:26 +0200

    feat: fibonacci worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,16 @@
+import { Worker } from "worker_threads";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPath = path.resolve("src/wt/worker.js");
+  const worker = new Worker(workerPath);
+  worker.postMessage(7); // 13
+  worker.postMessage(12); // 144
+  worker.postMessage(15); // 610
+
+  worker.on("message", (result) => {
+    console.log(result);
+  });
 };
 
 await performCalculations();
