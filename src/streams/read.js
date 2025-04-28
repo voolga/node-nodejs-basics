fcad20faa2244ed13ca70413c6d1f38232d6adba
Olga <olga.volotovich@mail.ru>
2025-04-28 22:26:14 +0200

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = "src/streams/files/fileToRead.txt";
+
+  const readStream = createReadStream(fileToRead);
+
+  readStream.pipe(process.stdout);
+
+  readStream.on("error", (error) => {
+    throw new Error(error.message);
+  });
+
+  readStream.on("end", () => {
+    console.log("\nEnd of file");
+  });
 };
 
 await read();
